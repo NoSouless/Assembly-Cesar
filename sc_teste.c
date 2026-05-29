906587f7dbaebe8ae27738541d721dd8ac02681d
Gabriela Correia <92410343+gabscorreia@users.noreply.github.com>
2026-05-28 21:42:51 -0300

    Update sc_teste.c
    teste

--- sc_teste.c
+++ sc_teste.c
@@ -1,6 +1,48 @@
 #include <stdio.h>
+#include <string.h>
 
-void cifra_cesar(palavra, chave){
+int main() {
+   
+    char criptografado[] = "fzaqhdkz drsz drstczmcn\n";
+    char palavrapossivel[] = "estuda"; 
+    char criptografiaatual[50];       
+    
+    char *ptr_orig = palavrapossivel;
+    char *ptr_dest = criptografiaatual;
 
+   
+    while (*ptr_orig != '\0') {
+        *ptr_dest = *ptr_orig + 1; 
+        ptr_orig++;
+        ptr_dest++;
+    }
+    *ptr_dest = '\0'; 
 
+  
+    int encontrado = 0;
+    int tam_frase = strlen(criptografado);
+    int tam_palavra = strlen(criptografiaatual);
+
+    for (int i = 0; i <= tam_frase - tam_palavra; i++) {
+        int bate = 1;
+        for (int j = 0; j < tam_palavra; j++) {
+            if (criptografado[i + j] != criptografiaatual[j]) {
+                bate = 0;
+                break;
+            }
+        }
+        if (bate) {
+            encontrado = 1;
+            break;
+        }
+    }
+
+    
+    if (encontrado) {
+        printf("Encontrado!\n");
+    } else {
+        printf("Não encontrado!\n");
+    }
+
+    return 0;
 }
