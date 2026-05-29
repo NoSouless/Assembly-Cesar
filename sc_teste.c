b2ed4eaa66d8846c8bcdeba3bcada9a0af4cbbcd
Gabriela Correia <92410343+gabscorreia@users.noreply.github.com>
2026-05-28 22:05:06 -0300

    Teste de Conversão em C
    Teste de Conversão em C

--- sc_teste.c
+++ sc_teste.c
@@ -2,31 +2,33 @@
 #include <string.h>
 
 int main() {
-   
-    char criptografado[] = "fzaqhdkz drsz drstczmcn\n";
-    char palavrapossivel[] = "estuda"; 
-    char criptografiaatual[50];       
-    
+
+    char criptografado[] = "gFrgh Fdhvdu\n";
+    char palavrapossivel[] = "gFrgh";
+    char criptografiaatual[3];
+
     char *ptr_orig = palavrapossivel;
     char *ptr_dest = criptografiaatual;
 
-   
+
     while (*ptr_orig != '\0') {
-        *ptr_dest = *ptr_orig + 1; 
+        *ptr_dest = *ptr_orig + 1;
         ptr_orig++;
         ptr_dest++;
     }
-    *ptr_dest = '\0'; 
+    *ptr_dest = '\0';
 
-  
+
     int encontrado = 0;
     int tam_frase = strlen(criptografado);
     int tam_palavra = strlen(criptografiaatual);
-
-    for (int i = 0; i <= tam_frase - tam_palavra; i++) {
+    int i;
+    int j;
+    int bate;
+    for ( i = 0; i <= tam_frase - tam_palavra; i++) {
         int bate = 1;
-        for (int j = 0; j < tam_palavra; j++) {
-            if (criptografado[i + j] != criptografiaatual[j]) {
+        for (j = 0; j < tam_palavra; j++) {
+            if (criptografado[i + j] + 1 != criptografiaatual[j]) {
                 bate = 0;
                 break;
             }
@@ -37,11 +39,11 @@ int main() {
         }
     }
 
-    
+
     if (encontrado) {
         printf("Encontrado!\n");
     } else {
-        printf("Não encontrado!\n");
+        printf("Nao encontrado!\n");
     }
 
     return 0;
