5ec39a9289e0b0d10c28d58de844e9e87712dcce
TechGust KBT <callmidade@gmail.com>
2026-06-04 16:09:59 -0300

    Criação do projeto de Cifra de Cesar em C

--- sc_teste.c
+++ sc_teste.c
@@ -1,50 +1,60 @@
-#include <stdio.h>
-#include <string.h>
-
 int main() {
 
-    char criptografado[] = "gFrgh Fdhvdu\n";
-    char palavrapossivel[] = "gFrgh";
-    char criptografiaatual[3];
+    char palavraOriginal[] = "ai";
+    char mensagem[] = "dkwy ks";
+    char buffer[100];
 
-    char *ptr_orig = palavrapossivel;
-    char *ptr_dest = criptografiaatual;
+    int saltos = 0;
 
+    while (saltos < 26) {
 
-    while (*ptr_orig != '\0') {
-        *ptr_dest = *ptr_orig + 1;
-        ptr_orig++;
-        ptr_dest++;
-    }
-    *ptr_dest = '\0';
+        // copia string
+        int i = 0;
+        while (palavraOriginal[i] != '\0') {
+            buffer[i] = palavraOriginal[i];
+            i++;
+        }
+        buffer[i] = '\0';
 
+        // aplica shift César
+        i = 0;
+        while (buffer[i] != '\0') {
+            buffer[i] = buffer[i] + saltos;
 
-    int encontrado = 0;
-    int tam_frase = strlen(criptografado);
-    int tam_palavra = strlen(criptografiaatual);
-    int i;
-    int j;
-    int bate;
-    for ( i = 0; i <= tam_frase - tam_palavra; i++) {
-        int bate = 1;
-        for (j = 0; j < tam_palavra; j++) {
-            if (criptografado[i + j] + 1 != criptografiaatual[j]) {
-                bate = 0;
+            if (buffer[i] > 'z') {
+                buffer[i] = buffer[i] - 26;
+            }
+            i++;
+        }
+
+        // busca substring (corrigida)
+        int j = 0;
+        int encontrou = 0;
+
+        while (mensagem[j] != '\0') {
+
+            int k = 0;
+
+            while (mensagem[j + k] != '\0' &&
+                   buffer[k] != '\0' &&
+                   mensagem[j + k] == buffer[k]) {
+                k++;
+            }
+
+            if (buffer[k] == '\0') {
+                encontrou = 1;
                 break;
             }
+
+            j++;
         }
-        if (bate) {
-            encontrado = 1;
+
+        if (encontrou) {
             break;
         }
+
+        saltos++;
     }
 
-
-    if (encontrado) {
-        printf("Encontrado!\n");
-    } else {
-        printf("Nao encontrado!\n");
-    }
-
-    return 0;
+    return saltos;
 }
